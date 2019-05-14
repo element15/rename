d30711308a283788749d737726921d1a7ae87a54
Christian Moomaw <element15@phosfor.us>
2019-05-14 11:58:12 +0200

    Add multipattern rename functionality

--- rename.py
+++ rename.py
@@ -41,6 +41,32 @@ format <replace_pattern> refer to #re.sub on the aforementioned doc page.
 
 https://docs.python.org/3/library/re.html
 """
+multipattern_help_epilogue = """\
+
+MORE DETAILS
+
+IMPORTANT: Backslash ('\\') characters in the pattern file must be properly
+escaped.
+
+The given JSON files must be formatted as an array of arrays. The outer array
+contains the collection of pattern pairs, and each inner array represents one
+pattern pair. Additionally, multiple string replacements may be allowed for a
+given pattern pair by adding a third value to the inner array equal to 'm',
+'multi', or 'multiple', similar to specifying the `-m` flag for single pattern
+replacement. PseudoJSON and an example follow:
+
+[
+	['search_pattern_1', 'replace_pattern_1', 'm'],
+	['search_pattern_2', 'replace_pattern_2'],
+	['search_pattern_3', 'replace_pattern_3', 'multi']
+]
+
+[
+    ['([\\\\d \\\\-]_spam_eggs\\\\.txt', 'spam_eggs_\\\\g<1>.txt'],
+    ['bakedbeans', 'spam', 'm'],
+    ['(\\\\d+)_', '\\\\g<1>-', 'm']
+]
+"""
 date_help_epilogue = """\
 
 MORE DETAILS
@@ -99,6 +125,20 @@ def main():
 	pattern_parser.add_argument('files', metavar='file', type=str, nargs='+',
 		help='file to be renamed')
 
+	multipattern_parser = subparsers.add_parser('multipattern',
+		help=('rename files based on multiple regex files loaded from a JSON '
+			'file'), epilog=multipattern_help_epilogue,
+		aliases=['m', 'mp', 'multi'],
+		formatter_class=argparse.RawDescriptionHelpFormatter)
+	multipattern_parser.set_defaults(func=multipattern)
+
+	multipattern_parser.add_argument('-d', '--dry-run', action='store_true',
+		help="perform a dry run; don't actually rename anything")
+	multipattern_parser.add_argument('pattern_file', metavar='<pattern_file>',
+		type=str, nargs=1, help='source JSON pattern file')
+	multipattern_parser.add_argument('files', metavar='file', type=str,
+		nargs='+',help='file to be renamed')
+
 	date_parser = subparsers.add_parser('date',
 		help=('reformat any dates found in filenames to conform to ISO 8601 '
 			'(yyyy-mm-dd)'), epilog=date_help_epilogue, aliases=['d'],
@@ -136,6 +176,36 @@ def pattern(args):
 		[f, re.sub(p, args.replace[0], f, (0 if args.multiple else 1))]
 		for f in args.files]
 	execute_rename(rename_pairs, dry_run=args.dry_run)
+
+def multipattern(args):
+	# Execute many pattern-based renames, as defined in a given JSON-formatted
+	# pattern file. Refer to the help pages for more details on the required
+	# JSON formatting.
+	pattern_pairs = import_patterns(args.pattern_file[0])
+
+	# executing multiple rename operations results in a bit of increased
+	# complexity compared to running only one pattern replacement. Sometimes a
+	# filename may match multiple patterns. As such, the rename_pairs list will
+	# be pre-populated with all of the given files, with a trivial rename
+	# (i.e. the before and after names will be identical). Each time the
+	# patterning loop is run, it will look at the *after* filenames, changing
+	# them accordingly.
+	#
+	# The end result should be a list of rename pairs which do
+	# not contain any double-renames or repeats. However, there may be some
+	# trivial renames in the list, which will need to be removed at the end.
+	rename_pairs = [[i, i] for i in args.files]
+	for pair in pattern_pairs:
+		# Decide whether this pattern should allow multiple replacements
+		multiple = (False if len(pair) < 3 else
+			(pair[2].lower() in ['m', 'multi', 'multiple']))
+		p = re.compile(pair[0])
+		for f in rename_pairs:
+			f[1] = re.sub(p, pair[1], f[1], (0 if multiple else 1))
+	# Remove trivial rename pairs
+	rename_pairs = [i for i in rename_pairs if i[0] != i[1]]
+	execute_rename(rename_pairs, dry_run=args.dry_run)
+
 
 def date(args):
 	# Execute a date-reformatting rename
@@ -302,5 +372,12 @@ def remove_collisions(rename_pairs):
 		raise ValueError('Collision removal algorithm failure')
 
 	return rename_pairs
+
+def import_patterns(filename):
+	# Load an appropriately-formatted JSON object from `filename` which contains
+	# a set of regex find-and-replace patterns
+	from json import loads
+	data = loads(open(filename).read())
+	return data
 
 main()
